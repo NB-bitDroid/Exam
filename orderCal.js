933c8b921ae018fadcfa248f48dab36bb688b449
NB-bitDroid <BennettNicolasM>
2026-09-17 14:35:16 -0400

    new stuff

--- orderCal.js
+++ orderCal.js
@@ -4,12 +4,12 @@ const itemQuantity = document.getElementById("quantityInput");
 function calTotal(){
     let subTotal = (Number(itemPrice.value) * Number(itemQuantity.value));
     if (subTotal >= 50) {
-        let discount = (subTotal*0.1)
-        let total = (subTotal - discount)
+        let discount = (subTotal*0.1);
+        let total = (subTotal - discount);
         document.getElementById("finalPrice").textContent = (`Prince: $${Number(itemPrice.value)}.00 and quantity: ${Number(itemQuantity.value)} 
         Subtotal: $${subTotal}.00   Discount: $${discount}.00  Total: $${total}.00`);
     } else {
         document.getElementById("finalPrice").textContent = (`Prince: $${Number(itemPrice.value)}.00 and quantity: ${Number(itemQuantity.value)}        
         Subtotal: $${subTotal}.00   Discount: $0.00  Total:  $${subTotal}.00`);
-    }
+    };
 };
---
+++ random_num.js
@@ -0,0 +1,5 @@
+function random_number(){
+    let random_num = (Math.floor(Math.random() * 10) + 1);
+    console.log(random_num)
+}
+random_number()
