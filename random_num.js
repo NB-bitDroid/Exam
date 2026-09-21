2c12c422eeb66f1dd0fcf0d02b77c2543e594eeb
NB-bitDroid <BennettNicolasM>
2026-09-21 12:45:59 -0400

    Number Guess game

--- random_num.js
+++ random_num.js
@@ -1,5 +1,32 @@
-function random_number(){
-    let random_num = (Math.floor(Math.random() * 10) + 1);
-    console.log(random_num)
-}
-random_number()
+const submitGuessBtn = document.getElementById("submitBtn");
+const guessCloseness = document.getElementById("clossnesOfTheGuess");
+const rest_btn = document.getElementById("newBtn");
+let random_num = (Math.floor(Math.random() * 10) + 1);
+let closeness_of_guesses = "";
+let numOfGuesses = 0;
+
+submitGuessBtn.addEventListener("click", () => {
+    numOfGuesses++;
+    let finalGuess = document.getElementById("guess");
+    if (Number(finalGuess.value) == random_num) {
+        closeness_of_guesses = (`Guess: ${numOfGuesses} - Correct!`);
+    } if (Number(finalGuess.value) < random_num) {
+        closeness_of_guesses = (`Guess: ${numOfGuesses} - Too low!`);
+    } if (Number(finalGuess.value) > random_num) {
+        closeness_of_guesses = (`Guess: ${numOfGuesses} - Too high!`);
+    };
+    updatestuff();
+
+});
+
+rest_btn.addEventListener("click", () => {
+    numOfGuesses = 0;
+    random_num = (Math.floor(Math.random() * 10) + 1);
+    closeness_of_guesses = "";
+    updatestuff();
+});
+
+function updatestuff(){
+    guessCloseness.innerText = closeness_of_guesses;
+    random_num.innerText = random_num;
+};
